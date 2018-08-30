a9e62601dbb649eb8052fa49e4b97186d04875f0
simmi9 <bhawna.keswani20@gmail.com>
2018-08-30 12:51:44 -0400

    Message board get-redirect-post working perfectly, Using JSON for API data

--- MessageBoard/MessageBoard_part3/test.py
+++ MessageBoard/MessageBoard_part3/test.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 #
-# Test script for the Messageboard Part Two server.
+# Test script for the Messageboard Part Three server.
 #
 # The server should be listening on port 8000, answer a GET request with
-# an HTML document, and answer a POST request with an echo of the "message"
-# field.
+# an HTML document, and answer a POST request with a redirect to the
+# main page.
 
 import requests, random, socket
 
@@ -19,13 +19,13 @@ def test_connect():
     except socket.error:
         return "Server didn't answer on localhost port 8000.  Is it running?"
 
-def test_POST():
-    '''The server should accept a POST and return the "message" field.'''
-    print("Testing POST request.")
+def test_POST_303():
+    '''The server should accept a POST and return a 303 to /.'''
+    print("Testing POST request, looking for redirect.")
     mesg = random.choice(["Hi there!", "Hello!", "Greetings!"])
     uri = "http://localhost:8000/"
     try:
-        r = requests.post(uri, data = {'message': mesg})
+        r = requests.post(uri, data = {'message': mesg}, allow_redirects=False)
     except requests.RequestException as e:
         return ("Couldn't communicate with the server. ({})\n"
                 "If it's running, take a look at its output.").format(e)
@@ -33,12 +33,13 @@ def test_POST():
         return ("The server returned status code 501 Not Implemented.\n"
                 "This means it doesn't know how to handle a POST request.\n"
                 "(Is the correct server code running?)")
-    elif r.status_code != 200:
-        return ("The server returned status code {} instead of a 200 OK."
+    elif r.status_code != 303:
+        return ("The server returned status code {} instead of a 303 redirect."
                 ).format(r.status_code)
-    elif r.text != mesg:
-        return ("The server sent a 200 OK response, but the content differed.\n"
-                "I expected '{}' but it sent '{}'.").format(mesg, r.text)
+    elif r.headers['location'] != '/':
+        return ("The server sent a 303 redirect to the wrong location."
+                "I expected '/' but it sent '{}'.").format(
+                    r.headers['location'])
     else:
         print("POST request succeeded.")
         return None
@@ -67,8 +68,24 @@ def test_GET():
         print("GET request succeeded!")
         return None
 
+def test_memory():
+    '''The server should remember posts.'''
+    print("Testing whether messageboard saves messages.")
+    uri = "http://localhost:8000"
+    mesg = random.choice(["Remember me!", "Don't forget.", "You know me."])
+    r = requests.post(uri, data = {'message': mesg})
+    if r.status_code != 200:
+        return ("Got status code {} instead of 200 on Post-Redirect-Get."
+                ).format(r.status_code)
+    elif mesg not in r.text:
+        return ("I posted a message but it didn't show up.\n"
+                "Expected '{}' to appear, but got this output instead:\n"
+                "{}").format(mesg, r.text)
+    else:
+        print("Post-Redirect-Get succeeded and I saw my message!")
+
 if __name__ == '__main__':
-    tests = [test_connect, test_POST, test_GET]
+    tests = [test_connect, test_POST_303, test_GET, test_memory]
     for test in tests:
         problem = test()
         if problem is not None:
